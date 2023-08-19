7d2017717dc011a2e3a8efefc9fd49775a5a1a39
HeatGub <ptrchm@proton.me>
2023-08-19 18:25:34 +0200

    Smoothly spawning asteroids

--- dist/script.js
+++ dist/script.js
@@ -12,18 +12,39 @@ window.addEventListener('load', function () {
             //accessible game object
             this.game = game;
             this.radius = 75; //18:33
-            this.x = Math.random() * this.game.width;
+            this.x = -this.radius; //to hide it on spawn
             this.y = Math.random() * this.game.width;
             this.image = document.getElementById('asteroid');
             this.spriteWidth = 150;
             this.spriteHeight = 150;
+            this.speed = 1 + Math.random();
+            // free to use in object pool, not animated
+            this.free = true;
         }
         draw(context) {
-            context.beginPath();
-            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-            context.stroke();
-            //draw source image
-            context.drawImage(this.image, this.x - this.spriteWidth / 2, this.y - this.spriteHeight / 2, this.spriteWidth, this.spriteHeight);
+            if (!this.free) {
+                context.beginPath();
+                context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+                context.stroke();
+                //draw source image
+                context.drawImage(this.image, this.x - this.spriteWidth / 2, this.y - this.spriteHeight / 2, this.spriteWidth, this.spriteHeight);
+            }
+        }
+        update() {
+            if (!this.free) {
+                this.x += this.speed;
+                if (this.x > this.game.width + this.radius) {
+                    this.reset();
+                }
+            }
+        }
+        reset() {
+            this.free = true;
+            this.x = -this.radius;
+            this.y = Math.random() * this.game.height;
+        }
+        start() {
+            this.free = false;
         }
     }
     //objects will be reused. Instead of being created and deleted they are just turned back to the initial state. Therefore garbage collection wont even have to happen.
@@ -33,6 +54,8 @@ window.addEventListener('load', function () {
             this.height = height;
             this.asteroidPool = [];
             this.maxAsteroids = 10; //can by a dynamic value
+            this.asteroidTimer = 0;
+            this.asteroidInterval = 1000;
             this.createAsteroidPool();
         }
         //a pool of active and inactiev objects
@@ -41,15 +64,46 @@ window.addEventListener('load', function () {
                 this.asteroidPool.push(new Asteroid(this));
             }
         }
-        render(context) {
-            this.asteroidPool.forEach(asteroid => asteroid.draw(context));
+        getElement() {
+            for (let i = 0; i < this.asteroidPool.length; i++) {
+                if (this.asteroidPool[i].free) {
+                    return this.asteroidPool[i];
+                }
+            }
+            return; //silences TS empty code paths error
+        }
+        render(context, deltaTime) {
+            //create asteroid periodically
+            if (this.asteroidTimer > this.asteroidInterval) {
+                const asteroid = this.getElement();
+                asteroid === null || asteroid === void 0 ? void 0 : asteroid.start();
+                //add new Asteroid
+                this.asteroidTimer = 0;
+            }
+            else {
+                this.asteroidTimer += deltaTime;
+            }
+            this.asteroidPool.forEach(asteroid => {
+                asteroid.draw(context);
+                asteroid.update();
+            });
             // this.asteroid.draw(context)
         }
     }
     // construct an instance of Game
     const game = new Game(canvas.width, canvas.height);
-    game.render(ctx);
-    // function animate() {
-    // }
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        // console.log(timeStamp)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(ctx, deltaTime);
+        // console.log(1000/deltaTime)
+        // console.log(deltaTime)
+        requestAnimationFrame(animate);
+    }
+    //run animation loop, first time stamp as argument
+    animate(0);
 });
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -16,24 +16,51 @@ window.addEventListener('load', function() {
         image: CanvasImageSource
         spriteWidth: number
         spriteHeight: number
+        speed: number
+        free: boolean
 
         constructor(game:Game){
             //accessible game object
             this.game = game
             this.radius = 75 //18:33
-            this.x = Math.random()* this.game.width
+            this.x = -this.radius //to hide it on spawn
             this.y = Math.random()* this.game.width
             this.image = document.getElementById('asteroid') as CanvasImageSource
             this.spriteWidth = 150
             this.spriteHeight = 150
+            this.speed = 1 + Math.random()
+            // free to use in object pool, not animated
+            this.free = true
         }
 
         draw(context:CanvasRenderingContext2D){
-            context.beginPath()
-            context.arc(this.x, this.y, this.radius, 0, Math.PI *2)
-            context.stroke()
-            //draw source image
-            context.drawImage(this.image, this.x - this.spriteWidth/2, this.y - this.spriteHeight/2, this.spriteWidth, this.spriteHeight)
+            if (!this.free){
+                context.beginPath()
+                context.arc(this.x, this.y, this.radius, 0, Math.PI *2)
+                context.stroke()
+                //draw source image
+                context.drawImage(this.image, this.x - this.spriteWidth/2, this.y - this.spriteHeight/2, this.spriteWidth, this.spriteHeight)
+            }
+
+        }
+
+        update(){
+            if (!this.free){
+                this.x += this.speed
+                if (this.x > this.game.width + this.radius){
+                    this.reset()
+                }
+            }
+        }
+
+        reset(){
+            this.free = true
+            this.x = -this.radius
+            this.y = Math.random() * this.game.height
+        }
+
+        start(){
+            this.free = false
         }
     }
 
@@ -44,12 +71,16 @@ window.addEventListener('load', function() {
         // asteroid: Asteroid
         asteroidPool: Asteroid[]
         maxAsteroids: number
+        asteroidTimer: number
+        asteroidInterval: number
 
         constructor(width:number, height:number){
             this.width = width
             this.height = height
             this.asteroidPool = []
             this.maxAsteroids = 10 //can by a dynamic value
+            this.asteroidTimer = 0
+            this.asteroidInterval = 1000
             this.createAsteroidPool()
         }
         //a pool of active and inactiev objects
@@ -59,19 +90,47 @@ window.addEventListener('load', function() {
             }
         }
 
-        render(context:CanvasRenderingContext2D){
-            this.asteroidPool.forEach( asteroid =>
+        getElement() {
+            for (let i=0;  i< this.asteroidPool.length; i++){
+                if (this.asteroidPool[i].free){
+                    return this.asteroidPool[i]
+                }
+            }
+            return //silences TS empty code paths error
+        }
+
+        render(context:CanvasRenderingContext2D, deltaTime:number){
+            //create asteroid periodically
+            if (this.asteroidTimer > this.asteroidInterval){
+                const asteroid = this.getElement()
+                asteroid?.start()
+                //add new Asteroid
+                this.asteroidTimer = 0
+            } else {
+                this.asteroidTimer += deltaTime
+            }
+            this.asteroidPool.forEach( asteroid => {
                 asteroid.draw(context)
-            )
+                asteroid.update()
+            })
             // this.asteroid.draw(context)
         }
     }
 
     // construct an instance of Game
     const game = new Game(canvas.width, canvas.height)
-    game.render(ctx)
-
-    // function animate() {
-
-    // }
+    
+    let lastTime = 0
+    function animate(timeStamp: number) {
+        const deltaTime = timeStamp - lastTime
+        lastTime = timeStamp
+        // console.log(timeStamp)
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        game.render(ctx, deltaTime)
+        // console.log(1000/deltaTime)
+        // console.log(deltaTime)
+        requestAnimationFrame(animate)
+    }
+    //run animation loop, first time stamp as argument
+    animate(0)
 })
